4b28f5380b09e21283ecc3751eec06a570d5a336
Alexander Kholodov <kholodov.alexander1@gmail.com>
2026-05-11 16:23:25 -0500

    Firebase App Hosting

---
+++ functions/lib/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.menuSuggestion = void 0;
+// Export all functions
+var genkit_sample_1 = require("./genkit-sample");
+Object.defineProperty(exports, "menuSuggestion", { enumerable: true, get: function () { return genkit_sample_1.menuSuggestion; } });
+//# sourceMappingURL=index.js.map
---
+++ functions/src/index.ts
@@ -0,0 +1,2 @@
+// Export all functions
+export { menuSuggestion } from './genkit-sample';
